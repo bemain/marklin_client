699427272be46363cfb5cb6fcc2cf83c871b5544
BeMain <benjamin.agardh@gmail.com>
2020-12-25 21:34:49 +0100

    Move BluetoothDeviceTile widget

--- lib/find_devices.dart
+++ lib/find_devices.dart
@@ -97,3 +97,40 @@ class FindDevicesScreenState extends State<FindDevicesScreen> {
     );
   }
 }
+
+class BluetoothDeviceTile extends StatelessWidget {
+  const BluetoothDeviceTile({Key key, this.device, this.onTap})
+      : super(key: key);
+
+  final BluetoothDevice device;
+  final VoidCallback onTap;
+
+  Widget _buildTitle(BuildContext context) {
+    if (device.name.length > 0) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            device.name,
+            overflow: TextOverflow.ellipsis,
+          ),
+          Text(
+            device.id.toString(),
+            style: Theme.of(context).textTheme.caption,
+          )
+        ],
+      );
+    } else {
+      return Text(device.id.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: _buildTitle(context),
+      onPressed: onTap,
+    );
+  }
+}
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,42 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
-
-class BluetoothDeviceTile extends StatelessWidget {
-  const BluetoothDeviceTile({Key key, this.device, this.onTap})
-      : super(key: key);
-
-  final BluetoothDevice device;
-  final VoidCallback onTap;
-
-  Widget _buildTitle(BuildContext context) {
-    if (device.name.length > 0) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            device.name,
-            overflow: TextOverflow.ellipsis,
-          ),
-          Text(
-            device.id.toString(),
-            style: Theme.of(context).textTheme.caption,
-          )
-        ],
-      );
-    } else {
-      return Text(device.id.toString());
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FlatButton(
-      child: _buildTitle(context),
-      onPressed: onTap,
-    );
-  }
-}
 
 class InfoScreen extends StatelessWidget {
   const InfoScreen({Key key, this.icon, this.text}) : super(key: key);
