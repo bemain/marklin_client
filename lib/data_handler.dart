8b1b298f2fbb2a1417d24cad7c01406570d2d9c5
BeMain <benjamin.agardh@gmail.com>
2020-12-25 21:16:15 +0100

    Add RaceHandler class

---
+++ lib/data_handler.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+/// Helper class for creating, deleting and updating races
+/// on the Firestore database.
+class RaceHandler {
+  CollectionReference races = FirebaseFirestore.instance.collection("races");
+  DocumentReference _race;
+  Stream<DocumentSnapshot> stream;
+
+  // Getters + Setters
+  DocumentReference get race => _race;
+  set race(DocumentReference newRace) {
+    _race = newRace;
+    stream = newRace.snapshots();
+  }
+
+  set raceByName(String name) => race = races.doc(name);
+
+  // Constructors
+  RaceHandler(raceName) {
+    raceByName = raceName;
+  }
+
+  // Laps
+  Future<void> addLap(int carID, double lapTime) async {
+    var doc = await race.get();
+    var newTimes = doc.data()["$carID"] + [lapTime];
+
+    await race.update({"$carID": newTimes});
+  }
+
+  Future<void> clearLaps() async {
+    await race.update({"0": [], "1": []});
+  }
+
+  /// Creates new race on the database and switches to it
+  Future<void> createRace() async {
+    race = await races.add({"dateTime": Timestamp.now(), "0": [], "1": []});
+  }
+}
