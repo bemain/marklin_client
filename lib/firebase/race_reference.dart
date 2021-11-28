861b4f222884db70d5a88d2d1e662b3c66767f66
BeMain <bemain.dev@gmail.com>
2021-11-28 16:28:29 +0100

    Update documentation

--- lib/firebase/race_reference.dart
+++ lib/firebase/race_reference.dart
@@ -3,22 +3,29 @@ import 'package:marklin_bluetooth/firebase/car_reference.dart';
 import 'package:marklin_bluetooth/firebase/lap.dart';
 import 'package:marklin_bluetooth/firebase/race.dart';
 
+/// Helper class for interacting with a Race on the database.
 class RaceReference {
   RaceReference({required this.docRef});
 
+  /// The Race Document on firebase that [this] references.
   final DocumentReference<Race> docRef;
 
-  Future<Race> get() async => (await docRef.get()).data()!;
+  Future<DocumentSnapshot<Race>> get() async => await docRef.get();
 
+  /// The Race that [this] references.
+  Future<Race> get race async => (await get()).data()!;
+
+  /// The reference to the car with id [carID],
+  /// on the Race that [this] references.
   CarReference carRef(int carID) =>
       CarReference(collRef: docRef.collection("$carID"));
 
-  /// Whether the current race is running or not.
+  /// Whether the Race that [this] references is running or not.
   Stream<bool> get runningStream =>
       docRef.snapshots().map((doc) => doc.get("running"));
 
   /// Add time since last lap, or since the race was started if no laps have
-  /// been run, to lap times of [carID] on the current race.
+  /// been run, to lap times of [carID] on the Race that [this] references.
   Future addLap(int carID, {double? lapTime, int? lapN}) async {
     if (carID >= (await get()).nCars) {
       return; // Trying to add lap to car not in race
@@ -45,7 +52,7 @@ class RaceReference {
     ));
   }
 
-  /// Delete all laps on this race
+  /// Delete all laps on the Race that [this] references.
   Future clear() async {
     for (var carID = 0; carID < (await get()).nCars; carID++) {
       for (var lap in (await carRef(carID).lapsRef.get()).docs) {
--- lib/firebase/races.dart
+++ lib/firebase/races.dart
@@ -7,11 +7,7 @@ import 'package:marklin_bluetooth/firebase/race_reference.dart';
 ///
 /// Requires Firebase to be initalized (Firebase.initializeApp()).
 class Races {
-  /// Get the collection that contains all laps for the car with id [carID]
-  /// on the current race.
-  static CollectionReference carCollection(carID) =>
-      currentRaceDoc.collection("$carID");
-
+  /// All the races on the database
   static CollectionReference<Race> races =
       FirebaseFirestore.instance.collection("races").withConverter<Race>(
             fromFirestore: (snapshot, _) => Race.fromJson(snapshot.data()!),
@@ -21,7 +17,7 @@ class Races {
   static DocumentReference<Race> currentRaceDoc = races.doc("current");
   static RaceReference currentRaceRef = RaceReference(docRef: currentRaceDoc);
 
-  /// Copy current race to a new race, then clear current laps
+  /// Copy current race to a new race, then clear the current race.
   static Future saveCurrentRace() async {
     var race = (await currentRaceDoc.get()).data()!;
     race.date = Timestamp.now();
