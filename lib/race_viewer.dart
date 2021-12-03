e6e3929193917499ec16cca1c7749c7757628a74
BeMain <bemain.dev@gmail.com>
2021-12-03 08:44:02 +0100

    Create widget LapsViewer

--- lib/race_viewer.dart
+++ lib/race_viewer.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:marklin_bluetooth/firebase/car_reference.dart';
+import 'package:marklin_bluetooth/firebase/lap.dart';
 import 'package:marklin_bluetooth/firebase/race.dart';
 import 'package:marklin_bluetooth/firebase/race_reference.dart';
 import 'package:marklin_bluetooth/utils.dart';
@@ -23,7 +25,7 @@ class RaceViewer extends StatelessWidget {
             children: List.generate(
               race.nCars * 2 - 1,
               (i) => i.isEven
-                  ? Expanded(child: lapViewer(raceRef, i ~/ 2))
+                  ? Expanded(child: LapsViewer(carRef: raceRef.carRef(i ~/ 2)))
                   : const VerticalDivider(thickness: 1.0),
             ),
           );
@@ -31,26 +33,32 @@ class RaceViewer extends StatelessWidget {
       ),
     );
   }
+}
 
-  Widget lapViewer(RaceReference raceRef, int carID) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: raceRef.docRef
-          .collection("$carID")
-          .orderBy("date", descending: true)
-          .snapshots(),
+class LapsViewer extends StatelessWidget {
+  final CarReference carRef;
+  final Function(DocumentReference<Lap> lapRef)? onLapSelected;
+
+  const LapsViewer({Key? key, required this.carRef, this.onLapSelected})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Lap>>(
+      stream: carRef.lapsRef.orderBy("date", descending: true).snapshots(),
       builder: niceAsyncBuilder(
         loadingText: "Getting lap times...",
         activeBuilder: (BuildContext c, snapshot) {
-          List<QueryDocumentSnapshot> docs = snapshot.data!.docs;
+          List<QueryDocumentSnapshot<Lap>> docs = snapshot.data!.docs;
           return ListView(
-            children: docs
-                .map(
-                  (doc) => TextTile(
-                    title: "${doc.get("lapNumber")} | ${doc.get("lapTime")}s",
-                    text: dateString((doc.get("date") as Timestamp).toDate()),
-                  ),
-                )
-                .toList(),
+            children: docs.map((lapSnap) {
+              Lap lap = lapSnap.data();
+              return TextTile(
+                title: "${lap.lapNumber} | ${lap.lapTime}s",
+                text: dateString((lap.date).toDate()),
+                onTap: () => onLapSelected?.call(lapSnap.reference),
+              );
+            }).toList(),
           );
         },
       ),
